8f4703104fa40962d6305dc269bd0853917f9f07
jhpark <jhintmain@gmail.com>
2024-11-28 23:18:52 +0900

    feat : livewire 설치

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\CreatePost;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
+Route::get('/creat-post', CreatePost::class);
